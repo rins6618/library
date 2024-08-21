14e85277ab5bffa1c3aeef05b6564a2e21102b01
rins <47487086+rins6618@users.noreply.github.com>
2024-08-20 21:28:56 -0300

    add book dialog modal, remove and read buttons

--- library.js
+++ library.js
@@ -7,11 +7,11 @@ const dialog = new DialogBox();
 
 booksAdded.updateDisplay = function () {
     const shelving = document.querySelector("main>ul");
+    shelving.replaceChildren();
     let shelves = Array.from(shelving.children);
     for (let idx in booksAdded) {
         if (Number.isNaN(Number(idx))) continue;
         let activeShelf;
-
         for (let shelf of shelves) {
             console.log(shelf.dataset.numBooks);
             if (parseInt(shelf.dataset.numBooks) < 7) {
@@ -25,20 +25,11 @@ booksAdded.updateDisplay = function () {
         shelves = Array.from(shelving.children);
         
         activeShelf.dataset.numBooks++;
-        booksAdded[idx].element = createBookElement(idx, activeShelf);
+        createBookElement(idx, activeShelf);
     }
     
 }
 
-
-/** 
- * @typedef {Object} Book
- * @property {string} name - Name of the book.
- * @property {string} author - Author of the book.
- * @property {number} pageCount - Page count of the book.
- * @property {boolean} isRead - If the book been read or not.
- * @property {Element?} element - HTML DOM element of the book.
- */
 
 
 /**
@@ -54,6 +45,7 @@ function Book(name, author, pageCount, isRead) {
     this.pageCount = pageCount;
     this.isRead = isRead;
     this.element = undefined;
+    this.color = undefined;
 }
 
 function DialogBox() {
@@ -61,23 +53,90 @@ function DialogBox() {
     
     dialogElement.classList.add("dialog-popup");
     document.querySelector("body").appendChild(dialogElement);
+    /** @type {HTMLDialogElement} element*/
     this.element = dialogElement;
     this.isModal = true;
     
-    this.changeParagraph = function(text) {
-        this.element.textContent = "";
-        const paragraphElement = document.createElement("p");
-        paragraphElement.innerText = text;
-        this.element.appendChild(paragraphElement);
+    /**
+     * 
+     * @param {Book} bookObject 
+     */
+    this.modalBook = function(bookObject) { 
+        this.element.replaceChildren();
+        const infoUL = document.createElement("ul");
+        infoUL.classList.add("grid", "dialog-info-list");
+        this.element.appendChild(infoUL);
+
+        for (let key in bookObject) {
+            let value = bookObject[key];
+            const ignoreList = ["element", "color"];
+            if (ignoreList.includes(key)) continue;
+            const listElem = document.createElement("li");
+            const contentElem = document.createElement("div");
+            listElem.textContent = convertPropertyName(key);
+            listElem.appendChild(contentElem);
+            listElem.dataset.keyValue = key;
+            contentElem.textContent = value;
+            listElem.classList.add("flex");
+            infoUL.appendChild(listElem);
+        }
+
+        const buttonUL = document.createElement("ul");
+        const removeButton = document.createElement("button");
+        const readButton = document.createElement("button");
+
+        buttonUL.classList.add("flex", "dialog-button-list");
+
+        removeButton.textContent = "Remove Book";
+        readButton.textContent = bookObject.isRead ? "Mark as unread" : "Mark as read";
+        this.element.appendChild(buttonUL);
+        buttonUL.appendChild(removeButton);
+        buttonUL.appendChild(readButton);
+
+        removeButton.addEventListener("click", e => {
+            let idx = booksAdded.indexOf(bookObject);
+            booksAdded.splice(idx, 1);
+            this.element.close();
+            booksAdded.updateDisplay();
+        });
+
+        readButton.addEventListener("click", e => {
+            bookObject.isRead = !bookObject.isRead;
+            readButton.textContent = bookObject.isRead ? "Mark as unread" : "Mark as read";
+            for (let listElem of infoUL.children) {
+                if (listElem.dataset.keyValue != 'isRead') continue;
+                const infoDiv = listElem.querySelector("div");
+                infoDiv.textContent = bookObject.isRead;
+            }
+        });
+        
+
+    }
+
+    this.formBook = function() {
+        this.element.replaceChildren();
+        const formElem = document.createElement("form");
+        const nameInput = document.createElement("input");
+        const authorInput = document.createElement("input");
+        const pageInput = document.createElement("input");
+        const readInput = document.createElement("input");
+
     }
 
     this.popup = function() {
-        if (this.isModal) {
-            this.element.showModal();
-        } else {
-            this.element.show();
-        }
+        this.element.showModal();
     }
+}
+
+/** @param {string} property */
+function convertPropertyName(property) {
+    const keyLabels = new Map([
+        ["name", "Book Name: "],
+        ["author", "Author: "], 
+        ["pageCount", "No. of pages: "], 
+        ["isRead", "Been read: "]
+    ]);
+    return keyLabels.has(property) ? keyLabels.get(property) : null;
 }
 
 function createShelf (shelving){
@@ -91,6 +150,8 @@ function createShelf (shelving){
 const COLORS = ["red", "green", "orange", "purple", "blue"];
 
 function createBookElement (bookIdx, shelf) {
+
+    const bookObj = booksAdded[bookIdx];
     const bookElement = document.createElement("div");
     bookElement.dataset.bookIdx = bookIdx;
     bookElement.classList.add("flex", "book");
@@ -100,14 +161,21 @@ function createBookElement (bookIdx, shelf) {
     const coverRElement = document.createElement("div");
     coverRElement.classList.add("cover", "right");
     const randomColor = Math.floor(Math.random() * COLORS.length);
-    coverLElement.style.setProperty("--book-color", COLORS[randomColor]);
-    coverRElement.style.setProperty("--book-color", COLORS[randomColor]);
+
+    let color = COLORS[randomColor];
+
+    if (bookObj.color !== undefined) {
+        color = bookObj.color;
+    }
+    bookObj.color = color;
+    coverLElement.style.setProperty("--book-color", color);
+    coverRElement.style.setProperty("--book-color", color);
     
     const pagesElement = document.createElement("div");
     pagesElement.classList.add("pages", "flex");
     
     const numPages = 4 + Math.floor(Math.random() * 8);
-    pagesElement.style.setProperty("--book-color", COLORS[randomColor]);
+    pagesElement.style.setProperty("--book-color", color);
     pagesElement.style.alignItems = "center";
     for (let i = 0; i < numPages; i++) {
         const pageHeight = 91 + Math.round(Math.random() * 3);
@@ -125,16 +193,11 @@ function createBookElement (bookIdx, shelf) {
     bookElement.appendChild(coverRElement);
 
     /** @type {Book}*/
-    const bookObj = booksAdded[bookIdx];
+    bookObj.element = bookElement;
 
 
     bookElement.addEventListener("click", e => {
-        const bookParagraphStr =
-       `Book Name: "${bookObj.name}"\n
-        Author: "${bookObj.author}"\n
-        Number of pages: ${bookObj.pageCount}\n
-        Has been read: ${bookObj.isRead ? "yes" : "no"}\n`;
-        dialog.changeParagraph(bookParagraphStr);
+        dialog.modalBook(bookObj);
         dialog.popup();
     });
 
@@ -142,6 +205,20 @@ function createBookElement (bookIdx, shelf) {
 }
 
 
-booksAdded.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, false), new Book("Where the Red Fern Grows", "WIlson Rawls and Clare Vanderpool", 314, true), new Book("A Dog's Way Home", "W. Bruce Cameron", 141, true));
+booksAdded.push(
+    new Book("The Hobbit", "J.R.R. Tolkien", 295, false), 
+    new Book("Where the Red Fern Grows", "WIlson Rawls and Clare Vanderpool", 314, false), 
+    new Book("A Dog's Way Home", "W. Bruce Cameron", 141, true),
+
+    new Book("The Hobbit", "J.R.R. Tolkien", 295, false), 
+    new Book("Where the Red Fern Grows", "WIlson Rawls and Clare Vanderpool", 314, false), 
+    new Book("A Dog's Way Home", "W. Bruce Cameron", 141, true)
+);
 
 booksAdded.updateDisplay();
+
+const addBookBtn = document.querySelector("#form-button");
+addBookBtn.addEventListener("click", e => {
+    dialog.formBook();
+    dialog.popup();
+})
